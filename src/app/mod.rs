d2543bff27e6857b2f5421694d1a4c0efeffbe6a
sak96 <26397224+sak96@users.noreply.github.com>
2024-04-21 21:36:20 +0530

    Rectangle tool actually handles the drawing logic

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,13 +1,57 @@
 mod canvas;
 mod shapes;
 mod tools;
+use web_sys::HtmlCanvasElement;
 use yew::prelude::*;
+
+use crate::app::{canvas::get_event_canvas_postion, shapes::Draw};
+
+macro_rules! move_to_current_scope {
+    ($( $i:ident ),*) => {
+        $(let $i = $i.clone();)*
+    };
+}
+
+macro_rules! handle_canvas_by_tool {
+    ($onevent:ident, $canvas_ref:expr, $shapes:expr, $tools:expr, $cur_tool:expr) => {
+        let $onevent = {
+            let canvas_ref = $canvas_ref.clone();
+            let shapes = $shapes.clone();
+            let tools = $tools.clone();
+            let cur_tool = $cur_tool.clone();
+            Callback::from(move |event: MouseEvent| {
+                let mut tools = tools.borrow_mut();
+                if *cur_tool > tools.len() {
+                    cur_tool.set(0)
+                }
+                let tool = &mut tools[*cur_tool];
+                let canvas: HtmlCanvasElement = canvas_ref.cast::<HtmlCanvasElement>().unwrap();
+                let position = get_event_canvas_postion(canvas.clone(), event);
+                tool.$onevent(position, canvas, &mut shapes.borrow_mut())
+            })
+        };
+    };
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
     let tools: Vec<Box<dyn tools::Tool>> = vec![Box::<tools::RectangleTool>::default()];
     let tools = use_mut_ref(|| tools);
+    let canvas_ref = use_node_ref();
+    let shapes = use_mut_ref(Vec::<Box<dyn Draw>>::new);
     let cur_tool = use_state_eq(|| 0);
+
+    handle_canvas_by_tool! {onmousedown, canvas_ref, shapes, tools, cur_tool};
+    handle_canvas_by_tool! {onmouseup, canvas_ref, shapes, tools, cur_tool};
+    handle_canvas_by_tool! {onmousemove, canvas_ref, shapes, tools, cur_tool};
+    let onresize = {
+        move_to_current_scope!(canvas_ref, shapes);
+        Callback::from(move |_| {
+            let canvas: HtmlCanvasElement = canvas_ref.cast::<HtmlCanvasElement>().unwrap();
+            canvas::refresh_canvas(canvas, &shapes.borrow());
+        })
+    };
+
     html! {
         <div style="min-height: 100vh; display: flex;">
             <div style=r#"
@@ -29,7 +73,14 @@ pub fn app() -> Html {
                 }}).collect::<Html>()
             }
             </div>
-            <canvas::DrawingCanvas />
+            <canvas
+                style="flex: 1"
+                ref={canvas_ref}
+                {onmouseup}
+                {onmousemove}
+                {onmousedown}
+                {onresize}
+                />
         </div>
     }
 }
--- src/app/tools.rs
+++ src/app/tools.rs
@@ -1,14 +1,35 @@
 use web_sys::HtmlCanvasElement;
 
-use super::shapes::Draw;
+use super::{
+    canvas::refresh_canvas,
+    shapes::{BBox, Draw, Rectangle},
+};
 
 #[allow(unused_variables)]
 pub trait Tool {
     fn button_icon(&self) -> &'static str;
     fn button_title(&self) -> &'static str;
-    fn on_mouse_down(&mut self, position: (f64,f64), canvas: HtmlCanvasElement, shapes: &mut Vec<Box<dyn Draw>>) {}
-    fn on_mouse_up(&mut self, position: (f64,f64), canvas: HtmlCanvasElement, shapes: &mut Vec<Box<dyn Draw>>) {}
-    fn on_mouse_move(&mut self, position: (f64,f64), canvas: HtmlCanvasElement, shapes: &mut Vec<Box<dyn Draw>>) {}
+    fn onmousedown(
+        &mut self,
+        position: (f64, f64),
+        canvas: HtmlCanvasElement,
+        shapes: &mut Vec<Box<dyn Draw>>,
+    ) {
+    }
+    fn onmouseup(
+        &mut self,
+        position: (f64, f64),
+        canvas: HtmlCanvasElement,
+        shapes: &mut Vec<Box<dyn Draw>>,
+    ) {
+    }
+    fn onmousemove(
+        &mut self,
+        position: (f64, f64),
+        canvas: HtmlCanvasElement,
+        shapes: &mut Vec<Box<dyn Draw>>,
+    ) {
+    }
 }
 
 #[derive(Default)]
@@ -23,5 +44,43 @@ impl Tool for RectangleTool {
 
     fn button_title(&self) -> &'static str {
         "Rectangle drawing tool."
+    }
+
+    fn onmousedown(
+        &mut self,
+        position: (f64, f64),
+        canvas: HtmlCanvasElement,
+        shapes: &mut Vec<Box<dyn Draw>>,
+    ) {
+        self.start.replace(position);
+        shapes.push(Box::new(Rectangle::new(position.0, position.1, 0.0, 0.0)));
+        refresh_canvas(canvas, shapes);
+    }
+
+    fn onmouseup(
+        &mut self,
+        position: (f64, f64),
+        canvas: HtmlCanvasElement,
+        shapes: &mut Vec<Box<dyn Draw>>,
+    ) {
+        if let (Some(shape), Some(start)) = (shapes.last_mut(), self.start) {
+            shape.resize_to_bbox(BBox::from_corner(start, position));
+            refresh_canvas(canvas, shapes);
+        }
+        self.start = None
+    }
+
+    fn onmousemove(
+        &mut self,
+        position: (f64, f64),
+        canvas: HtmlCanvasElement,
+        shapes: &mut Vec<Box<dyn Draw>>,
+    ) {
+        if let (Some(shape), Some(start)) = (shapes.last_mut(), self.start) {
+            shape.resize_to_bbox(BBox::from_corner(start, position));
+            refresh_canvas(canvas, shapes);
+        } else {
+            self.start = None
+        }
     }
 }
