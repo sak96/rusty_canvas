ef8645b673958a557d9dff4ae8955a0a3c62300c
sak96 <26397224+sak96@users.noreply.github.com>
2024-04-21 16:03:35 +0530

    Add toolbar

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,9 +1,18 @@
 mod canvas;
+mod toolbar;
 use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let tool = use_state_eq(|| &toolbar::TOOLS[0]);
+    let ontoolchange = {
+        let tool = tool.clone();
+        Callback::from(move |new_tool| tool.set(new_tool))
+    };
     html! {
-        <canvas::DrawingCanvas />
+        <div>
+            <toolbar::Toolbar {ontoolchange} />
+            <canvas::DrawingCanvas />
+        </div>
     }
 }
