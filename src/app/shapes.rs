8899cb8551ac2ee26512374b1c43a500dfe14a5c
sak96 <26397224+sak96@users.noreply.github.com>
2024-04-26 21:05:28 +0530

    Selection done.

--- src/app/shapes.rs
+++ src/app/shapes.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::JsValue;
 use web_sys::CanvasRenderingContext2d;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub struct BBox {
     pub left: f64,
     pub top: f64,
@@ -18,12 +18,53 @@ impl BBox {
             height: (y - y1).abs(),
         }
     }
+
+    fn right(&self) -> f64 {
+        self.left + self.width
+    }
+
+    fn bottom(&self) -> f64 {
+        self.top + self.height
+    }
+
+    #[must_use]
+    pub fn in_(&self, bbox: &BBox) -> bool {
+        self.left >= bbox.left
+            && self.top >= bbox.top
+            && self.right() <= bbox.right()
+            && self.bottom() <= bbox.bottom()
+    }
+
+    pub fn add_padding(&mut self, padding: f64) {
+        self.left -= padding;
+        self.top -= padding;
+        self.width += padding + padding;
+        self.height += padding + padding;
+    }
+
+    pub fn add_bbox(&mut self, bbox: &BBox) {
+        let left = self.left.min(bbox.left);
+        let top = self.top.min(bbox.top);
+        self.width = self.right().max(bbox.right()) - left;
+        self.height = self.bottom().max(bbox.bottom()) - top;
+        self.left = left;
+        self.top = top;
+    }
 }
 
 pub trait Draw {
     fn bbox(&self) -> BBox;
     fn draw(&self, context: &CanvasRenderingContext2d);
-    fn resize_to_bbox(&mut self, bbox: BBox) -> bool;
+    fn _resize_to_bbox(&mut self, bbox: &BBox);
+
+    fn resize_to_bbox(&mut self, bbox: &BBox) -> bool {
+        if &self.bbox() != bbox {
+            self._resize_to_bbox(bbox);
+            true
+        } else {
+            false
+        }
+    }
 }
 
 #[derive(Default)]
@@ -32,6 +73,7 @@ pub struct Rectangle {
     top: f64,
     width: f64,
     height: f64,
+    pub is_selection: bool,
 }
 
 impl Draw for Rectangle {
@@ -45,20 +87,19 @@ impl Draw for Rectangle {
     }
 
     fn draw(&self, context: &CanvasRenderingContext2d) {
-        context.set_stroke_style(&JsValue::from_str("green"));
+        if self.is_selection {
+            context.set_stroke_style(&JsValue::from_str("blue"));
+        } else {
+            context.set_stroke_style(&JsValue::from_str("green"));
+        }
         context.stroke_rect(self.left, self.top, self.width, self.height);
     }
 
-    fn resize_to_bbox(&mut self, bbox: BBox) -> bool {
-        if self.bbox() != bbox {
-            self.left = bbox.left;
-            self.top = bbox.top;
-            self.width = bbox.width;
-            self.height = bbox.height;
-            true
-        } else {
-            false
-        }
+    fn _resize_to_bbox(&mut self, bbox: &BBox) {
+        self.left = bbox.left;
+        self.top = bbox.top;
+        self.width = bbox.width;
+        self.height = bbox.height;
     }
 }
 
@@ -97,15 +138,10 @@ impl Draw for Ellipse {
         context.stroke();
     }
 
-    fn resize_to_bbox(&mut self, bbox: BBox) -> bool {
-        if self.bbox() != bbox {
-            self.radius_x = bbox.width / 2.0;
-            self.radius_y = bbox.height / 2.0;
-            self.center_x = bbox.left + self.radius_x;
-            self.center_y = bbox.top + self.radius_y;
-            true
-        } else {
-            false
-        }
+    fn _resize_to_bbox(&mut self, bbox: &BBox) {
+        self.radius_x = bbox.width / 2.0;
+        self.radius_y = bbox.height / 2.0;
+        self.center_x = bbox.left + self.radius_x;
+        self.center_y = bbox.top + self.radius_y;
     }
 }
--- src/app/tools/select_tool.rs
+++ src/app/tools/select_tool.rs
@@ -5,7 +5,31 @@ use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 #[derive(Default)]
 pub struct SelectTool {
     start: Option<(f64, f64)>,
-    shape: Box<Rectangle>,
+    shape: Rectangle,
+    shapes: Vec<Rectangle>,
+}
+
+const PADDING: f64 = 5.0;
+
+impl SelectTool {
+    fn handle_selection(
+        &self,
+        selection: &BBox,
+        shapes: &mut Vec<Box<dyn Draw>>,
+    ) -> Vec<Rectangle> {
+        let mut selections = vec![];
+        for shape in shapes {
+            let mut bbox = shape.bbox();
+            if bbox.in_(selection) {
+                bbox.add_padding(PADDING);
+                let mut rect = Rectangle::default();
+                rect.resize_to_bbox(&bbox);
+                rect.is_selection = true;
+                selections.push(rect);
+            }
+        }
+        selections
+    }
 }
 
 #[allow(unused_variables)]
@@ -24,9 +48,10 @@ impl Tool for SelectTool {
         canvas: HtmlCanvasElement,
         shapes: &mut Vec<Box<dyn Draw>>,
     ) -> bool {
+        self.shape.is_selection = true;
         self.start.replace(position);
-        self.shape
-            .resize_to_bbox(BBox::from_corner(position, position));
+        let selected_area = BBox::from_corner(position, position);
+        self.shape.resize_to_bbox(&selected_area);
         true
     }
 
@@ -37,10 +62,17 @@ impl Tool for SelectTool {
         shapes: &mut Vec<Box<dyn Draw>>,
     ) -> bool {
         if let Some(start) = self.start.take() {
-            let mut shape = Box::<Rectangle>::default();
-            let changed = shape.resize_to_bbox(BBox::from_corner(start, position));
-            shapes.push(shape);
-            changed
+            let selection = BBox::from_corner(start, position);
+            self.shapes = self.handle_selection(&selection, shapes);
+            if let Some(first) = self.shapes.first() {
+                let mut bbox = first.bbox();
+                for shape in &self.shapes {
+                    bbox.add_bbox(&shape.bbox())
+                }
+                bbox.add_padding(2.0 * PADDING);
+                self.shape.resize_to_bbox(&bbox);
+            }
+            true
         } else {
             false
         }
@@ -53,14 +85,22 @@ impl Tool for SelectTool {
         shapes: &mut Vec<Box<dyn Draw>>,
     ) -> bool {
         if let Some(start) = self.start {
-            self.shape
-                .resize_to_bbox(BBox::from_corner(start, position))
+            let selection = BBox::from_corner(start, position);
+            self.shape.resize_to_bbox(&selection);
+            self.shapes = self.handle_selection(&selection, shapes);
+            true
         } else {
             false
         }
     }
 
     fn draw_extra_shapes(&self, context: &CanvasRenderingContext2d) {
-        self.shape.draw(context);
+        // if either some thing is selected or selection is happening
+        if !self.shapes.is_empty() || self.start.is_some() {
+            self.shape.draw(context);
+            for shape in &self.shapes {
+                shape.draw(context);
+            }
+        }
     }
 }
