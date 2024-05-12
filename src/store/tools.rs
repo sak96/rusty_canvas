5511c93117abe317702334b9b47ec56a01576757
sak96 <26397224+sak96@users.noreply.github.com>
2024-05-13 00:33:56 +0530

    Use store to store tools index

---
+++ src/store/tools.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::str::FromStr;
+use yewdux::prelude::*;
+
+use crate::types::tools::Tool;
+
+#[derive(Default, Clone, PartialEq, Eq, Serialize, Deserialize, Store)]
+#[store(storage = "local", storage_tab_sync)]
+pub struct Tools {
+    pub tool: Tool,
+}
+
+impl serde::Serialize for Tool {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+impl<'de> serde::Deserialize<'de> for Tool {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)
+            .unwrap_or_default()
+            .clone();
+        Ok(Self::from_str(&value).unwrap_or_default())
+    }
+}
--- src/types/tools/mod.rs
+++ src/types/tools/mod.rs
@@ -3,19 +3,37 @@ use enum_dispatch::enum_dispatch;
 use crate::store::shapes::Shapes;
 use crate::types::events::Event;
 use crate::types::shapes::Shape;
+
 pub mod select_tool;
 pub mod shape_tool;
 
 use select_tool::SelectTool;
 use shape_tool::{EllipseTool, RectangleTool};
 
+use strum_macros::{Display, EnumIter, EnumString};
+
 #[enum_dispatch(ToolAction)]
 #[allow(clippy::enum_variant_names)]
+#[derive(EnumString, EnumIter, Display, Clone)]
 pub enum Tool {
+    SelectTool,
     RectangleTool,
     EllipseTool,
-    SelectTool,
 }
+
+impl Default for Tool {
+    fn default() -> Self {
+        SelectTool {}.into()
+    }
+}
+
+impl PartialEq for Tool {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_string().eq(&other.to_string())
+    }
+}
+
+impl Eq for Tool {}
 
 #[allow(unused_variables)]
 #[enum_dispatch]
@@ -23,41 +41,4 @@ pub trait ToolAction {
     fn button_icon(&self) -> &'static str;
     fn button_title(&self) -> &'static str;
     fn handle_event(&mut self, event: &Event, shapes: &mut Shapes) -> Option<Shape>;
-}
-
-pub struct ToolBar {
-    tools: [Tool; 3],
-    tool_idx: usize,
-}
-
-impl ToolBar {
-    pub fn new() -> Self {
-        Self {
-            tools: [
-                SelectTool {}.into(),
-                RectangleTool::default().into(),
-                EllipseTool::default().into(),
-            ],
-            tool_idx: 0,
-        }
-    }
-
-    pub fn all_tools(&self) -> &[Tool] {
-        &self.tools
-    }
-
-    pub fn get_tool_idx(&self) -> usize {
-        self.tool_idx
-    }
-
-    pub fn set_tool_idx(&mut self, mut tool_idx: usize) {
-        if tool_idx >= self.tools.len() {
-            tool_idx = 0;
-        }
-        self.tool_idx = tool_idx
-    }
-
-    pub fn handle_event(&mut self, event: &Event, shapes: &mut Shapes) -> Option<Shape> {
-        self.tools[self.tool_idx].handle_event(event, shapes)
-    }
 }
