f4d645e996a305de2ad0ee7d974ae4e71dbf3c4a
sak96 <26397224+sak96@users.noreply.github.com>
2025-08-16 22:24:50 +0530

    Ignore errors on hover and click

--- src/types/events.rs
+++ src/types/events.rs
@@ -3,9 +3,13 @@ pub type Point = (f64, f64);
 #[non_exhaustive]
 pub enum CanvasEvent {
     PointerEventStart(Point),
+    /// currently not used any where
+    #[allow(dead_code)]
     Hover(Point),
     DragMove((Point, Point)),
     DragEnd((Point, Point)),
+    /// currently not used any where
+    #[allow(dead_code)]
     Click(Point),
     DeselectTool,
     SelectTool,
